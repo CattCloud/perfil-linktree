e6c841d875a3d74cbffd59d0118d4746690c0ccd
Erick Dreyk <141431682+CattCloud@users.noreply.github.com>
2025-02-05 20:12:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ p.textContent = "Animales tiernos con la capacidad de dominar el internet";
 // Añade los elementos al contenedor
 titulo.appendChild(h1);
 titulo.appendChild(p);
+
+// Obtener referencias a los elementos
+const miDiv1 = document.getElementById("container1");
+const miDiv2 = document.getElementById("container2");
+const miDiv3 = document.getElementById("container3");
+
+const miImagen = document.getElementById("img_perfil");
+
+miDiv1.addEventListener("click", function() {
+    miImagen.src = "img/gato3.png";
+  });
+miDiv2.addEventListener("click", function() {
+    miImagen.src = "img/gato4.jpg";
+});
+miDiv3.addEventListener("click", function() {
+    miImagen.src = "img/gato5.png";
+});
